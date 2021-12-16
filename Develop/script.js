53c54b4a07d042b1a7638371ee7e329cc316f982
Gabi Purnell <gabrielpurnell@gmail.com>
2021-12-16 18:48:12 +1100

    working on the prompts

--- Develop/script.js
+++ Develop/script.js
@@ -3,35 +3,60 @@ var generateBtn = document.querySelector("#generate");
 
 
 //have array of each type of character
-var specialChar = ["!@#$%^&*()_+=-,./?><:;"]
-var charUpper = ["QWERTYUIOPASDFGHJKLZXCVBNM"]
-var charLower = ["qwertyuiopasdfghjklzxcvbnm"]
-var charNumber = ["1234567890"]
+var special = ["!@#$%^&*()_+=-,./?><:;"]
+var upperCase = ["QWERTYUIOPASDFGHJKLZXCVBNM"]
+var lowerCase = ["qwertyuiopasdfghjklzxcvbnm"]
+var numbers = ["1234567890"]
 
+// Add event listener to generate button - given code
+generateBtn.addEventListener("click", writePassword);
 
 //set length of min 8, max 128
 
-var setLength = window.prompt ("Choose password length between 8 and 128 characters") 
-  if (setLength < 8 || setLength > 128)
-console.log = "passwordlength"
+var setLength = window.prompt ("Choose password length between 8 and 128 characters"); 
+  if (setLength === < 8 || setLength > 128)
+
+  
+console.log = "set password length"
   
 
+// Add event listener to generate button - given
 
-// Write password to the #password input - given
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword(){
+//use prompts to determine what characters the user wants to include in password.
+
+var useUpper = window.confirm ("Do you want to include uppercase letters in your password?");
+var useLower = window.confirm ("Do you want to include lowercase letters in your password?");
+var useSpecialcharacters = window.confirm ("Do you wany to include special characters in your password?");
+var useNumbers = window.confirm ("Do you want to include numbers in your password?")
 
 }
 
-// Add event listener to generate button - given
+console.log ("Using special characters?", useSpecialcharacters);
+console.log ("Using uppercase letters?", useUpper);
+console.log ("Using lowercase characters?", useLower);
+console.log ("Using numbers?", useNumbers);
+
+
+
+// Write password to the #password input - given
+
+
+// Add event listener to generate button - given code
 generateBtn.addEventListener("click", writePassword);
 
 
-// then do the prompts
-// pick one of each then randomly loop through the rest of the characters.
+
+
+// pick one of each then randomly loop through the rest of the characters.?
 //set length of min 8, max 128
 
 //1. need to enter choice options for password ie letters, numbers, special characters. uppercase or lowercase.
