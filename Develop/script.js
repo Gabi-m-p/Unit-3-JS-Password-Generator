1875d01eeafc5a2acecc873c45154c0a2fc5a3e8
Gabi Purnell <gabrielpurnell@gmail.com>
2021-12-16 18:05:45 +1100

    working on the prompts to ask users choice

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,23 @@
-// Assignment Code
+// Assignment Code - given
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+//have array of each type of character
+var specialChar = ["!@#$%^&*()_+=-,./?><:;"]
+var charUpper = ["QWERTYUIOPASDFGHJKLZXCVBNM"]
+var charLower = ["qwertyuiopasdfghjklzxcvbnm"]
+var charNumber = ["1234567890"]
+
+
+//set length of min 8, max 128
+
+var setLength = window.prompt ("Choose password length between 8 and 128 characters") 
+  if (setLength < 8 || setLength > 128)
+console.log = "passwordlength"
+  
+
+
+// Write password to the #password input - given
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,10 +26,13 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button - given
 generateBtn.addEventListener("click", writePassword);
 
 
+// then do the prompts
+// pick one of each then randomly loop through the rest of the characters.
+//set length of min 8, max 128
 
 //1. need to enter choice options for password ie letters, numbers, special characters. uppercase or lowercase.
 //2. Needs a prompt for length of password.
